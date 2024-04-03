61c57849b7eff29b79915041cb8939fc69f9dcf3
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-04-03 13:26:22 -0400

    Create BcsSalsifyBundle.php

---
+++ src/BcsSalsifyBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao Isotope Salsify
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-isotope-salsify
+ */
+
+namespace Bcs\SalsifyBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BcsSalsifyBundle extends Bundle
+{
+}
