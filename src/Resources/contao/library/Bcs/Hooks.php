5048c08c270bcf7477912da2bfda27ff4def126a
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-08-07 14:56:16 -0400

    Update Hooks.php

--- src/Resources/contao/library/Bcs/Hooks.php
+++ src/Resources/contao/library/Bcs/Hooks.php
@@ -2,17 +2,35 @@
 
 namespace Bcs;
 
+use pcrov\JsonReader\JsonReader;
+
 class Hooks
 {
     public function generatePage(&$objPageModel, $objLayout, &$objPage)
     {
-        
+
+        // If this is our "Contact - Success" test page
         if($objPageModel->id == 58)
         {
-            echo "<pre>";
-            print_r($objLayout);
-            echo "</pre>";
-            die();
+            //echo "<pre>";
+            //print_r($objLayout);
+            //echo "</pre>";
+            //die();
+
+            
+
+            $reader = new JsonReader();
+            $reader->open("salsify/product-feed.json");
+            
+            while ($reader->read("attribute")) {
+                echo $reader->value(), "\n";
+            }
+            
+            $reader->close();
+            
+            
+
+            
         }
         
     }
