0f8b54e689f0673f294bf9282aca0ae9eb8db629
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-08-08 15:33:18 -0400

    Update Hooks.php

--- src/Resources/contao/library/Bcs/Hooks.php
+++ src/Resources/contao/library/Bcs/Hooks.php
@@ -22,6 +22,14 @@ class Hooks
             $reader = new JsonReader();
             $reader->open("../salsify/product-feed-paper_2024_04_12_16_45_01_UTC.json");
 
+
+
+
+            
+            /************************/
+            /* Process "Attributes" */
+            /************************/
+            
             $reader->read("attributes");
             $depth = $reader->depth(); // Check in a moment to break when the array is done.
             
@@ -31,6 +39,13 @@ class Hooks
                 print_r($reader->value());
                 echo "</pre>";
             } while ($reader->next() && $reader->depth() > $depth); // Read each sibling.
+
+
+
+
+
+
+
             
             $reader->close();
             
