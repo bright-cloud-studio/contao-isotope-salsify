82fec3e881cbe69e4dfb0db6e3843261f69745d9
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-01-08 14:38:03 -0500

    Create modules.php

---
+++ src/Resources/contao/languages/en/modules.php
@@ -0,0 +1,5 @@
+<?php
+
+/* Back End modules */
+$GLOBALS['TL_LANG']['MOD']['salsify_request']               = array('Salsify Request', 'Logs of updates requested by Salsify');
+$GLOBALS['TL_LANG']['MOD']['salsify_attribute']             = array('Salsify Attribute', 'A Salsify key/value pair to be used in the parent Salsify Request');
