5ac172a2adb735418a4a357ca8c41b665c777b34
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-01-08 15:06:12 -0500

    Create tl_salsify_attribute.php

---
+++ src/Resources/contao/languages/en/tl_salsify_attribute.php
@@ -0,0 +1,15 @@
+<?php
+
+/* System Buttons */
+$GLOBALS['TL_LANG']['tl_salsify_attribute']['new']                        = array('New Salsify Attribute', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_salsify_attribute']['show']                       = array('Salsify Attribute Details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_salsify_attribute']['edit']                       = array('Edit Salsify Attribute', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_salsify_attribute']['copy']                       = array('Copy Salsify Attribute', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_salsify_attribute']['delete']                     = array('Delete Salsify Attribute', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_salsify_attribute']['toggle']                     = array('Toggle Salsify Attribute', 'Toggle record ID %s');
+
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_salsify_attrbute']['salsify_attribute_legend']    = 'Salsify Attribute Details';
+$GLOBALS['TL_LANG']['tl_salsify_attribute']['attribute_key']              = array('Attribute Key', 'The Attribute\'s KEY');
+$GLOBALS['TL_LANG']['tl_salsify_attribute']['attribute_value']            = array('Attribute Value', 'The Attribute\'s VALUE');
