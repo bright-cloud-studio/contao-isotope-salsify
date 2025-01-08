6c1daf645882cee39e7775d094b1c94ad6590789
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-01-08 13:41:08 -0500

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -10,3 +10,8 @@
 
 /** Hooks */
 $GLOBALS['TL_HOOKS']['generatePage'][] 		 = array('Bcs\Hooks', 'generatePage');
+
+/* Back end modules - Work DCAs */
+$GLOBALS['TL_LANG']['MOD']['salsify'][0] = "Salsify";
+$GLOBALS['BE_MOD']['salsify']['salsify_request'] = array( 'tables' => array('tl_salsify_request') );
+$GLOBALS['BE_MOD']['salsify']['salsify_attribute'] = array( 'tables' => array('tl_salsify_attribute') );
