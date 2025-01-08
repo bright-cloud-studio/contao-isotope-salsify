92164239ab319a594cbf73179cff1310c8fad868
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-01-08 11:19:21 -0500

    Update tl_salsify_request.php

--- src/Resources/contao/dca/tl_salsify_request.php
+++ src/Resources/contao/dca/tl_salsify_request.php
@@ -15,7 +15,7 @@ use Contao\MemberModel;
 use Contao\System;
 
 /* Table tl_price_chart */
-$GLOBALS['TL_DCA']['tl_assignment'] = array
+$GLOBALS['TL_DCA']['tl_salsify_request'] = array
 (
  
     // Config
@@ -25,13 +25,9 @@ $GLOBALS['TL_DCA']['tl_assignment'] = array
         'ctable'                      => array('tl_transaction'),
         'switchToEdit'                => false,
         'enableVersioning'            => true,
-        'onsubmit_callback' => array
-		(
-			array('Bcs\Backend\AssignmentBackend', 'createAssignment')
-		),
         'onload_callback' => array
 		(
-			array('tl_assignment', 'setRootType')
+			array('tl_salsify_request', 'setRootType')
 		),
         'sql' => array
         (
@@ -112,6 +108,7 @@ $GLOBALS['TL_DCA']['tl_assignment'] = array
     // Fields
     'fields' => array
     (
+        
         // Contao Fields
         'id' => array
         (
@@ -147,184 +144,8 @@ $GLOBALS['TL_DCA']['tl_assignment'] = array
 
         ),
 
-
-        // Transaction Fields
         
-        'date_created' => array
-        (
-            'label'                   => &$GLOBALS['TL_LANG']['tl_assignment']['date_created'],
-            'inputType'               => 'text',
-            'default'                 => '',
-            'filter'                  => true,
-            'search'                  => true,
-            'eval'                    => array('datepicker'=>true, 'mandatory'=>true, 'tl_class'=>'w50'),
-            'sql'                     => "varchar(20) NOT NULL default ''",
-            'default'                 => date("m/d/y"),
-        ),
-        'date_30_day' => array
-        (
-            'label'                   => &$GLOBALS['TL_LANG']['tl_assignment']['date_30_day'],
-            'inputType'               => 'text',
-            'default'                 => '',
-            'filter'                  => false,
-            'search'                  => false,
-            'eval'                    => array('datepicker'=>true, 'mandatory'=>false, 'tl_class'=>'w50'),
-            'sql'                     => "varchar(20) NOT NULL default ''",
-            'default'                 => "",
-        ),
-        'date_45_day' => array
-        (
-            'label'                   => &$GLOBALS['TL_LANG']['tl_assignment']['date_45_day'],
-            'inputType'               => 'text',
-            'default'                 => '',
-            'filter'                  => false,
-            'eval'                    => array('datepicker'=>true, 'mandatory'=>false, 'tl_class'=>'w50'),
-            'sql'                     => "varchar(20) NOT NULL default ''",
-            'default'                 => "",
-        ),
-        'psychologist' => array
-        (
-            'label'                   => &$GLOBALS['TL_LANG']['tl_assignment']['psychologist'],
-            'inputType'               => 'select',
-            'filter'                  => true,
-            'search'                  => true,
-            'flag'                    => DataContainer::SORT_ASC,
-            'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50', 'chosen'=>true, 'includeBlankOption'=>true, 'blankOptionLabel'=>'Select a Psychologist'),
-            'options_callback'	      => array('Bcs\Backend\AssignmentBackend', 'getPsychologists'),
-            'sql'                     => "varchar(255) NOT NULL default ''"
-        ),
-        'psychologists_shared' => array
-        (
-            'label'                   => &$GLOBALS['TL_LANG']['tl_assignment']['psychologists_shared'],
-            'inputType'               => 'checkbox',
-            'filter'                  => false,
-            'search'                  => false,
-            'flag'                    => DataContainer::SORT_ASC,
-            'eval'                    => array('multiple'=> true, 'mandatory'=>false, 'tl_class'=>'w50'),
-            'options_callback'	      => array('Bcs\Backend\AssignmentBackend', 'getPsychologistsShared'),
-            'sql' => "blob NULL"
-        ),
-        'district' => array
-        (
-            'label'                   => &$GLOBALS['TL_LANG']['tl_assignment']['district'],
-            'inputType'               => 'select',
-            'filter'                  => true,
-            'search'                  => true,
-            'flag'                    => DataContainer::SORT_ASC,
-            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50', 'chosen'=>true, 'submitOnChange'=>true),
-            'options_callback'	      => array('Bcs\Backend\AssignmentBackend', 'getDistricts'),
-            'sql'                     => "varchar(255) NOT NULL default ''"
-        ),
-        'school' => array
-        (
-            'label'                   => &$GLOBALS['TL_LANG']['tl_assignment']['school'],
-            'inputType'               => 'select',
-            'filter'                  => true,
-            'search'                  => true,
-            'flag'                    => DataContainer::SORT_ASC,
-            'foreignKey'              => 'tl_school.school_name',
-            'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50', 'chosen'=>true),
-            'options_callback'	      => array('Bcs\Backend\AssignmentBackend', 'getSchools'),
-            'sql'                     => "varchar(255) NOT NULL default ''"
-        ),
-        'student' => array
-        (
-            'label'                   => &$GLOBALS['TL_LANG']['tl_assignment']['student'],
-            'inputType'               => 'select',
-            'filter'                  => true,
-            'search'                  => true,
-            'flag'                    => DataContainer::SORT_ASC,
-            'foreignKey'              => 'tl_student.name',
-            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50', 'chosen'=>true),
-            'options_callback'	      => array('Bcs\Backend\AssignmentBackend', 'getStudents'),
-            'sql'                     => "varchar(255) NOT NULL default ''"
-        ),
-
-        
-        
-        'initial_reeval' => array
-        (
-            'label'                   => &$GLOBALS['TL_LANG']['tl_assignment']['initial_reeval'],
-            'inputType'               => 'select',
-            'filter'                  => true,
-            'search'                  => false,
-            'default'                 => '',
-    		'eval'                     => array('mandatory'=>true, 'tl_class'=>'w50', 'chosen'=>true),
-    		'options_callback'	      => array('Bcs\Backend\AssignmentBackend', 'getInitialReeval'),
-            'sql'                      => "varchar(30) NOT NULL default ''"
-        ),
-        'type_of_testing' => array
-        (
-            'label'                   => &$GLOBALS['TL_LANG']['tl_assignment']['type_of_testing'],
-            'inputType'               => 'select',
-            'filter'                  => true,
-            'search'                  => false,
-            'flag'                    => DataContainer::SORT_ASC,
-            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50', 'chosen'=>true),
-            'options_callback'	      => array('Bcs\Backend\AssignmentBackend', 'getServices'),
-            'sql'                     => "varchar(255) NOT NULL default ''"
-        ),
-        'testing_date' => array
-        (
-            'label'                   => &$GLOBALS['TL_LANG']['tl_assignment']['testing_date'],
-            'inputType'               => 'text',
-            'default'                 => '',
-            'search'                  => false,
-            'filter'                  => false,
-            'eval'                    => array('datepicker'=>true, 'mandatory'=>false, 'tl_class'=>'w50'),
-            'sql'                     => "varchar(20) NOT NULL default ''",
-        ),
-        'meeting_required' => array
-        (
-            'label'                   => &$GLOBALS['TL_LANG']['tl_assignment']['meeting_required'],
-            'inputType'               => 'select',
-            'search'                  => false,
-            'filter'                  => false,
-            'default'                 => '',
-            'options'                  => array('yes' => 'Yes', 'no' => 'No'),
-    		'eval'                     => array('mandatory'=>false, 'tl_class'=>'w50', 'chosen'=>true, 'includeBlankOption'=>true, 'blankOptionLabel'=>'Select Yes/No'),
-    		'sql'                      => "varchar(10) NOT NULL default ''"
-        ),
-        'meeting_date' => array
-        (
-            'label'                   => &$GLOBALS['TL_LANG']['tl_assignment']['meeting_date'],
-            'inputType'               => 'text',
-            'default'                 => '',
-            'search'                  => false,
-            'filter'                  => false,
-            'eval'                    => array('datepicker'=>true, 'mandatory'=>false, 'tl_class'=>'w50'),
-            'sql'                     => "varchar(20) NOT NULL default ''",
-        ),
-        'contact_info_parent' => array
-        (
-            'label'                   => &$GLOBALS['TL_LANG']['tl_assignment']['contact_info_parent'],
-            'inputType'               => 'text',
-            'default'                 => '',
-            'search'                  => false,
-            'filter'                  => false,
-            'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50'),
-            'sql'                     => "text NOT NULL default ''"
-        ),
-        'contact_info_teacher' => array
-        (
-            'label'                   => &$GLOBALS['TL_LANG']['tl_assignment']['contact_info_teacher'],
-            'inputType'               => 'text',
-            'default'                 => '',
-            'search'                  => false,
-            'filter'                  => false,
-            'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50'),
-            'sql'                     => "text NOT NULL default ''"
-        ),
-        'team_chair' => array
-        (
-            'label'                   => &$GLOBALS['TL_LANG']['tl_assignment']['team_chair'],
-            'inputType'               => 'text',
-            'default'                 => '',
-            'search'                  => false,
-            'filter'                  => false,
-            'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50'),
-            'sql'                     => "varchar(255) NOT NULL default ''"
-        ),
+        // Salsify Request Fields
         'email' => array
         (
             'label'                   => &$GLOBALS['TL_LANG']['tl_assignment']['email'],
@@ -335,52 +156,6 @@ $GLOBALS['TL_DCA']['tl_assignment'] = array
             'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50'),
             'sql'                     => "varchar(255) NOT NULL default ''"
         ),
-        'report_submitted' => array
-        (
-            'label'                   => &$GLOBALS['TL_LANG']['tl_assignment']['report_submitted'],
-            'inputType'               => 'select',
-            'filter'                  => true,
-            'search'                  => false,
-            'default'                 => 'no',
-            'options'                  => array('no' => 'No', 'yes' => 'Yes'),
-    		'eval'                     => array('mandatory'=>true, 'tl_class'=>'w50', 'chosen'=>true),
-    		'sql'                      => "varchar(5) NOT NULL default ''"
-        ),
-        'notes' => array
-        (
-            'label'                   => &$GLOBALS['TL_LANG']['tl_assignment']['notes'],
-            'inputType'               => 'textarea',
-            'default'                 => '',
-            'search'                  => false,
-            'filter'                  => false,
-            'eval'                    => array('mandatory'=>false, 'tl_class'=>'clr w100'),
-            'sql'                     => "text NOT NULL default ''"
-        ),
-
-
-        // Hidden fields for search purposes
-        'lasid' => array
-        (
-            'label'                   => &$GLOBALS['TL_LANG']['tl_assignment']['lasid'],
-            'inputType'               => 'text',
-            'default'                 => '',
-            'filter'                  => true,
-            'search'                  => true,
-            'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50'),
-            'sql'                     => "varchar(255) NOT NULL default ''"
-        ),
-        'sasid' => array
-        (
-            'label'                   => &$GLOBALS['TL_LANG']['tl_assignment']['sasid'],
-            'inputType'               => 'text',
-            'default'                 => '',
-            'filter'                  => true,
-            'search'                  => true,
-            'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50'),
-            'sql'                     => "varchar(255) NOT NULL default ''"
-        ),
-
-        
         
         'published' => array
         (
@@ -394,7 +169,7 @@ $GLOBALS['TL_DCA']['tl_assignment'] = array
 );
 
 
-class tl_assignment extends Backend
+class tl_salsify_request extends Backend
 {
   
 	public function setRootType(DataContainer $dc)
