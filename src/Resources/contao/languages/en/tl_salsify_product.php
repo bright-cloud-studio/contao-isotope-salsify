ae7c911e60fffcf3b37e42e08e0758be67fdd9fc
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-01-09 15:28:39 -0500

    Update and rename tl_salsify_request.php to tl_salsify_product.php

---
+++ src/Resources/contao/languages/en/tl_salsify_product.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Bright Cloud Studio's GAI Invoices
+ *
+ * Copyright (C) 2022-2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/gai-invoices
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* System Buttons */
+$GLOBALS['TL_LANG']['tl_salsify_product']['new']                        = array('New Salsify Product', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_salsify_product']['show']                       = array('Salsify Product Details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_salsify_product']['edit']                       = array('Edit Salsify Product', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_salsify_product']['copy']                       = array('Copy Salsify Product', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_salsify_product']['delete']                     = array('Delete Salsify Product', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_salsify_product']['toggle']                     = array('Toggle Salsify Product', 'Toggle record ID %s');
+
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_salsify_product']['salsify_request_legend']     = 'Salsify Product Details';
+$GLOBALS['TL_LANG']['tl_salsify_product']['product_sku']                = array('Product SKU', 'The SKU of the product this Salsify Product is about');
+$GLOBALS['TL_LANG']['tl_salsify_product']['email']                      = array('Email Address', 'The Email Address that will receive notifications in the event of an unresolvable issue');
