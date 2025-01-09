0706724f04109a5727f516f96645f013488629a6
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-01-09 11:55:59 -0500

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -16,6 +16,9 @@ $GLOBALS['TL_LANG']['MOD']['salsify'][0] = "Salsify";
 $GLOBALS['BE_MOD']['salsify']['salsify_request'] = array( 'tables' => array('tl_salsify_request') );
 $GLOBALS['BE_MOD']['salsify']['salsify_attribute'] = array( 'tables' => array('tl_salsify_attribute') );
 
+/* Front End modules */
+$GLOBALS['FE_MOD']['salsify']['mod_salsify_importer']         = 'Bcs\Module\ModSalsifyImporter';
+
 /* Models */
 $GLOBALS['TL_MODELS']['tl_salsify_request']         = 'Bcs\Model\SalsifyRequest';
 $GLOBALS['TL_MODELS']['tl_salsify_attribute']         = 'Bcs\Model\SalsifyAttribute';
