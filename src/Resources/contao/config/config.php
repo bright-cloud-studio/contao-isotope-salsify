31fccf7ae6703c0273923a2b6d2d4acf858aa213
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-01-09 15:27:21 -0500

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -13,12 +13,12 @@ $GLOBALS['TL_HOOKS']['generatePage'][] 		 = array('Bcs\Hooks', 'generatePage');
 
 /* Back end modules - Work DCAs */
 $GLOBALS['TL_LANG']['MOD']['salsify'][0] = "Salsify";
-$GLOBALS['BE_MOD']['salsify']['salsify_request'] = array( 'tables' => array('tl_salsify_request') );
+$GLOBALS['BE_MOD']['salsify']['salsify_product'] = array( 'tables' => array('tl_salsify_product') );
 $GLOBALS['BE_MOD']['salsify']['salsify_attribute'] = array( 'tables' => array('tl_salsify_attribute') );
 
 /* Front End modules */
 $GLOBALS['FE_MOD']['salsify']['mod_salsify_importer']         = 'Bcs\Module\ModSalsifyImporter';
 
 /* Models */
-$GLOBALS['TL_MODELS']['tl_salsify_request']         = 'Bcs\Model\SalsifyRequest';
-$GLOBALS['TL_MODELS']['tl_salsify_attribute']         = 'Bcs\Model\SalsifyAttribute';
+$GLOBALS['TL_MODELS']['tl_salsify_product']         = 'Bcs\Model\SalsifyProduct';
+$GLOBALS['TL_MODELS']['tl_salsify_attribute']       = 'Bcs\Model\SalsifyAttribute';
