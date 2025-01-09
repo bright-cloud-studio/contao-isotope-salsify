4eb1222b38e76eea84a3668bb074d39498f3aa44
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-01-09 15:35:56 -0500

    Update modules.php

--- src/Resources/contao/languages/en/modules.php
+++ src/Resources/contao/languages/en/modules.php
@@ -1,5 +1,5 @@
 <?php
 
 /* Back End modules */
-$GLOBALS['TL_LANG']['MOD']['salsify_request']               = array('Salsify Request', 'Logs of updates requested by Salsify');
+$GLOBALS['TL_LANG']['MOD']['salsify_product']               = array('Salsify Product', 'A Salsify Product which holds attributes');
 $GLOBALS['TL_LANG']['MOD']['salsify_attribute']             = array('Salsify Attribute', 'A Salsify key/value pair to be used in the parent Salsify Request');
