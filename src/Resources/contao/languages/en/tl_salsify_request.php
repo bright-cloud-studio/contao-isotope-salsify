9e56ccb0ed9e5a6807b33047eb9453487785df00
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-01-10 14:04:40 -0500

    Create tl_salsify_request.php

---
+++ src/Resources/contao/languages/en/tl_salsify_request.php
@@ -0,0 +1,15 @@
+<?php
+
+/* System Buttons */
+$GLOBALS['TL_LANG']['tl_salsify_request']['new']                        = array('New Salsify Request', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_salsify_request']['show']                       = array('Salsify Request Details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_salsify_request']['edit']                       = array('Edit Salsify Request', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_salsify_request']['copy']                       = array('Copy Salsify Request', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_salsify_request']['delete']                     = array('Delete Salsify Request', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_salsify_request']['toggle']                     = array('Toggle Salsify Request', 'Toggle record ID %s');
+
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_salsify_request']['salsify_attribute_legend']   = 'Salsify Request Details';
+$GLOBALS['TL_LANG']['tl_salsify_request']['attribute_key']              = array('Request Name', 'An internal-only name used to identify in the Backend');
+$GLOBALS['TL_LANG']['tl_salsify_request']['attribute_value']            = array('Salsify XML Folder', 'Select the folder where the Salsify XML files can be found.');
