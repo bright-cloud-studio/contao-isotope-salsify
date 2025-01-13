2770e543fa746a478a24c62ba391c9e0e263494b
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-01-13 10:35:05 -0500

    Create ModSalsifyStatusUpdate.php

---
+++ src/Resources/contao/library/Bcs/Module/ModSalsifyStatusUpdate.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Bcs\Module;
+
+use Bcs\Model\SalsifyAttribute;
+use Bcs\Model\SalsifyProduct;
+use Bcs\Model\SalsifyRequest;
+
+use pcrov\JsonReader\JsonReader;
+
+use Contao\BackendTemplate;
+use Contao\System;
+use Contao\FrontendUser;
+
+
+class ModSalsifyStatusUpdate extends \Contao\Module
+{
+
+    /* Default Template */
+    protected $strTemplate = 'mod_salsify_status_update';
+
+    // Stores our messages, later displayed in the module's template
+    protected static $log = array();
+
+    /* Construct function */
+    public function __construct($objModule, $strColumn='main')
+    {
+        parent::__construct($objModule, $strColumn);
+    }
+
+    /* Generate function */
+    public function generate()
+    {
+        $request = System::getContainer()->get('request_stack')->getCurrentRequest();
+
+        if ($request && System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest($request))
+        {
+            $objTemplate = new BackendTemplate('be_wildcard');
+ 
+            $objTemplate->wildcard = '### ' . mb_strtoupper($GLOBALS['TL_LANG']['FMD']['salsify_importer'][0]) . ' ###';
+            $objTemplate->title = $this->headline;
+            $objTemplate->id = $this->id;
+            $objTemplate->link = $this->name;
+            $objTemplate->href = 'contao/main.php?do=themes&table=tl_module&act=edit&id=' . $this->id;
+ 
+            return $objTemplate->parse();
+        }
+ 
+        return parent::generate();
+    }
+
+
+    protected function compile()
+    {
+
+      echo "Hey yo";
+      die();
+      
+    }
+  
+}
