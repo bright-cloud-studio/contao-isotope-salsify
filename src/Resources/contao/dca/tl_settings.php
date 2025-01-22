b8707c2ff89b7285fccee35d6f9208c3460d6bd6
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-01-22 13:11:41 -0500

    Update tl_settings.php

--- src/Resources/contao/dca/tl_settings.php
+++ src/Resources/contao/dca/tl_settings.php
@@ -2,20 +2,11 @@
 
 use Contao\Config;
 
-$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = str_replace('{files_legend', '{salsify_legend}, name, category_parent_page, salsify_category_field;{files_legend', $GLOBALS['TL_DCA']['tl_settings']['palettes']['default']);
+$GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = str_replace('{files_legend', '{salsify_legend}, category_parent_page, salsify_category_field;{files_legend', $GLOBALS['TL_DCA']['tl_settings']['palettes']['default']);
 
 
 $GLOBALS['TL_DCA']['tl_settings']['fields'] += [
-
-    // Add a Select to pick the Model
-    'name' => [
-
-        'label'                   => &$GLOBALS['TL_LANG']['tl_settings']['name'],
-        'inputType'               => 'text',
-        'eval'                    => ['mandatory'=>false, 'tl_class'=>'w100'],
-        'sql'                     => "varchar(255) default ''"
-    ],
-
+    
     // Add an input box for the prompt
     'category_parent_page' => [
         'label'                   => &$GLOBALS['TL_LANG']['tl_settings']['category_parent_page'],
