abbfb32ee2cd9897c21c47d918ead6620102657b
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-01-22 12:48:40 -0500

    Update tl_settings.php

--- src/Resources/contao/dca/tl_settings.php
+++ src/Resources/contao/dca/tl_settings.php
@@ -10,7 +10,7 @@ $GLOBALS['TL_DCA']['tl_settings']['fields'] += [
     // Add a Select to pick the Model
     'name' => [
 
-        'label'                   => &$GLOBALS['TL_LANG']['tl_salsify_request']['name'],
+        'label'                   => &$GLOBALS['TL_LANG']['tl_settings']['name'],
         'inputType'               => 'text',
         'eval'                    => ['mandatory'=>false, 'tl_class'=>'w100'],
         'sql'                     => "varchar(255) default ''"
@@ -18,7 +18,7 @@ $GLOBALS['TL_DCA']['tl_settings']['fields'] += [
 
     // Add an input box for the prompt
     'category_parent_page' => [
-        'label'                   => &$GLOBALS['TL_LANG']['tl_salsify_request']['category_parent_page'],
+        'label'                   => &$GLOBALS['TL_LANG']['tl_settings']['category_parent_page'],
         'inputType'               => 'pageTree',
         'eval'                    => array('files'=>false, 'fieldType'=>'radio', 'multiple'=>true, 'tl_class'=>'w50'),
         'sql'                     => "blob NULL",
@@ -27,7 +27,7 @@ $GLOBALS['TL_DCA']['tl_settings']['fields'] += [
 
     // Add a Radio option to choose if we should automatically generate or not
     'salsify_category_field' => [
-        'label'                   => &$GLOBALS['TL_LANG']['tl_salsify_request']['salsify_category_field'],
+        'label'                   => &$GLOBALS['TL_LANG']['tl_settings']['salsify_category_field'],
         'inputType'               => 'text',
         'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50'),
         'sql'                     => "varchar(255) default ''"
