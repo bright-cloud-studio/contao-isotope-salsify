4c47b862cf083e66c994754e40a8aeaf448f0e03
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-01-22 12:46:29 -0500

    Update tl_settings.php

--- src/Resources/contao/dca/tl_settings.php
+++ src/Resources/contao/dca/tl_settings.php
@@ -8,42 +8,29 @@ $GLOBALS['TL_DCA']['tl_settings']['palettes']['default'] = str_replace('{files_l
 $GLOBALS['TL_DCA']['tl_settings']['fields'] += [
 
     // Add a Select to pick the Model
-    'openai_model' => [
-        'label'             => &$GLOBALS['TL_LANG']['tl_settings']['openai_model'],
-        'inputType'         => 'select',
-        'options_callback'  => function ()
-        {
-            return [
-                'gpt-4o-mini' => 'gpt-4o-mini',
-            ];
-        },
-        'default'           => 'gpt-4o-mini',
-        'eval'              => ['mandatory' => 'true', 'tl_class' => 'w50', 'chosen' => true, 'submitOnChange' => true],
+    'name' => [
+
+        'label'                   => &$GLOBALS['TL_LANG']['tl_salsify_request']['name'],
+        'inputType'               => 'text',
+        'eval'                    => ['mandatory'=>false, 'tl_class'=>'w100'],
+        'sql'                     => "varchar(255) default ''"
     ],
 
     // Add an input box for the prompt
-    'openai_prompt' => [
-        'label'             => &$GLOBALS['TL_LANG']['tl_settings']['openai_prompt'],
-        'inputType'         => 'text',
-        'default'           => 'Based on this image, create a search engine optimized alt text (under 15 words)',
-        'eval'              => ['mandatory' => 'true', 'tl_class' => 'w50'],
+    'category_parent_page' => [
+        'label'                   => &$GLOBALS['TL_LANG']['tl_salsify_request']['category_parent_page'],
+        'inputType'               => 'pageTree',
+        'eval'                    => array('files'=>false, 'fieldType'=>'radio', 'multiple'=>true, 'tl_class'=>'w50'),
+        'sql'                     => "blob NULL",
+        'relation'                => array('table'=>'tl_page', 'type'=>'hasMany', 'load'=>'lazy')
     ],
 
     // Add a Radio option to choose if we should automatically generate or not
-    'openai_automatic' => [
-        'label'             => &$GLOBALS['TL_LANG']['tl_settings']['openai_automatic'],
-        'inputType'         => 'radio',
-        'options'           => array('yes' => 'Yes', 'no' => 'No'),
-        'default'           => 'yes',
-        'eval'              => array('mandatory'=>true, 'tl_class'=>'w50'),
-    ],
-
-    // Lists file extensions that will trigger the ai
-    'openai_extensions' => [
-        'label'             => &$GLOBALS['TL_LANG']['tl_settings']['openai_extensions'],
-        'inputType'         => 'text',
-        'default'           => 'Based on this image, create a search engine optimized alt text (under 15 words)',
-        'eval'              => array('mandatory'=>true, 'tl_class'=>'w50', 'default'=> 'Based on this image, create a search engine optimized alt text (under 15 words)'),
+    'salsify_category_field' => [
+        'label'                   => &$GLOBALS['TL_LANG']['tl_salsify_request']['salsify_category_field'],
+        'inputType'               => 'text',
+        'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50'),
+        'sql'                     => "varchar(255) default ''"
     ],
     
 ];
