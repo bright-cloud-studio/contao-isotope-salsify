3528620fd99cbe5bd5fa3b7e59d8afe001ead44a
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-01-24 09:22:31 -0500

    Update tl_salsify_attribute.php

--- src/Resources/contao/languages/en/tl_salsify_attribute.php
+++ src/Resources/contao/languages/en/tl_salsify_attribute.php
@@ -13,3 +13,10 @@ $GLOBALS['TL_LANG']['tl_salsify_attribute']['toggle']                     = arra
 $GLOBALS['TL_LANG']['tl_salsify_attrbute']['salsify_attribute_legend']    = 'Salsify Attribute Details';
 $GLOBALS['TL_LANG']['tl_salsify_attribute']['attribute_key']              = array('Attribute Key', 'The Attribute\'s KEY');
 $GLOBALS['TL_LANG']['tl_salsify_attribute']['attribute_value']            = array('Attribute Value', 'The Attribute\'s VALUE');
+
+$GLOBALS['TL_LANG']['tl_salsify_attrbute']['options_legend']              = 'Attribute Options';
+$GLOBALS['TL_LANG']['tl_salsify_attribute']['linked_isotope_attribute']   = array('Linked Isotope Attribute', 'Select the Isotope Attribute this will be filled into.');
+$GLOBALS['TL_LANG']['tl_salsify_attribute']['site_category_field']        = array('Site Category', 'Choose if this Attribute is used for Page aliases.');
+
+$GLOBALS['TL_LANG']['tl_salsify_attrbute']['error_log_legend']            = 'INTERNAL DETAILS';
+$GLOBALS['TL_LANG']['tl_salsify_attribute']['error_log']                  = array('Error Log', 'If something goes the error message will be plased here.');
