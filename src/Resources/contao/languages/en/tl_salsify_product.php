889a07265d3fb44e4090e5b8768a2c84846ff954
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-01-27 10:00:13 -0500

    Update tl_salsify_product.php

--- src/Resources/contao/languages/en/tl_salsify_product.php
+++ src/Resources/contao/languages/en/tl_salsify_product.php
@@ -23,3 +23,6 @@ $GLOBALS['TL_LANG']['tl_salsify_product']['toggle']                     = array(
 $GLOBALS['TL_LANG']['tl_salsify_product']['salsify_product_legend']     = 'Salsify Product Details';
 $GLOBALS['TL_LANG']['tl_salsify_product']['product_sku']                = array('Product SKU', 'The SKU of the product this Salsify Product is about');
 $GLOBALS['TL_LANG']['tl_salsify_product']['email']                      = array('Email Address', 'The Email Address that will receive notifications in the event of an unresolvable issue');
+
+$GLOBALS['TL_LANG']['tl_salsify_product']['isotope_product_type']       = array('Isotope Product Type', 'Select the Isotope Product Type this Salsify Product will turn into');
+$GLOBALS['TL_LANG']['tl_salsify_product']['import_status']              = array('Import Status', 'Tracks the Impot Status of this product');
