4448d074fc4771e0fcb376ee1b35faac67bbccb3
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-01-27 11:41:04 -0500

    Update Hooks.php

--- src/Resources/contao/library/Bcs/Hooks.php
+++ src/Resources/contao/library/Bcs/Hooks.php
@@ -4,17 +4,28 @@ namespace Bcs;
 
 use ZipArchive;
 
-use Bcs\Model\SalsifyRequest;
 use Bcs\Model\SalsifyAttribute;
+use Bcs\Model\SalsifyProduct;
+use Bcs\Model\SalsifyRequest;
 
-use pcrov\JsonReader\JsonReader; // Json streaming library
+use pcrov\JsonReader\JsonReader;
+
+use Contao\BackendTemplate;
+use Contao\Controller;
 use Contao\Database;
-use Isotope\Model\Attribute;
-use Isotope\Model\Attribute\TextField;
-use Isotope\Model\AttributeOption;
+use Contao\System;
+use Contao\FrontendUser;
+
 use Isotope\Interfaces\IsotopeProduct;
 use Isotope\Isotope;
+use Isotope\DatabaseUpdater;
+
+use Isotope\Model\Attribute;
+use Isotope\Model\AttributeOption;
+use Isotope\Model\Attribute\TextField;
 use Isotope\Model\Product;
+
+use Isotope\Backend\Attribute\DatabaseUpdate;
 
 
 class Hooks
@@ -26,6 +37,46 @@ class Hooks
         {
 
             echo "generatePage CALLED <br>";
+
+
+            // Open and process file
+            $reader = new JsonReader();
+            $reader->open("../files/salsify/salsify_product_feed_2025_01_22_18_15_52_428_UTC.json");
+            $depth = $reader->depth();
+            $reader->read();
+            
+            $do_loop = 0;
+            // Process loaded XML data
+            do
+            {
+            	$do_loop++;
+            
+            	// Load the first array, which is the overall wrapper of arrays
+            	$array_parent = $reader->value();
+            
+            	$prod_count = 0;
+            	// Loop through children arrays, these are what store the actual values here
+            	foreach($array_parent as $array_child) {
+            		$prod_count++;
+            		
+            		// Create a Salsify Product to hold our Salsify Attributes
+            		$salsify_product = new SalsifyProduct();
+            		$salsify_product->tstamp = time();
+            		$salsify_product->product_sku = $do_loop . '_' . $prod_count;
+            		$salsify_product->name = 'product_' . $do_loop . '_' . $prod_count;
+            		$salsify_product->save();
+            
+            	}
+            
+            } while ($reader->next() && $reader->depth() > $depth); // Read each sibling.
+            
+            $reader->close();
+
+
+
+
+            
+            
             
         }
         
