e7997ffcb79706a04fa0d526d05dbf7531169857
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-01-27 14:23:21 -0500

    Update SalsifyProductBackend.php

--- src/Resources/contao/library/Bcs/Backend/SalsifyProductBackend.php
+++ src/Resources/contao/library/Bcs/Backend/SalsifyProductBackend.php
@@ -83,17 +83,5 @@ class SalsifyProductBackend extends Backend
 
 		return $varValue;
 	}
-
-    // Build an array with the KEY being the ID of the Isotope Attribute and the VALUE is the text-readable name
-    public function getIsotopeProductTypes()
-    {
-        $options = array();
-        $attributes = ProductType::findAll();
-        while($attributes->next()) {
-            $attr = $attributes->row();
-            $options[$attr['id']] = $attr['name'];
-        }
-        return $options;
-    }
     
 }
