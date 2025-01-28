e62ce979e5c0f867077b4e56dea2427d2ee67d16
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-01-28 14:28:59 -0500

    Update action.create.product.php

--- src/Resources/public/php/action.create.product.php
+++ src/Resources/public/php/action.create.product.php
@@ -12,12 +12,48 @@
     
     
     // Loop through the Salsify Products
-    
-    
-        // Make sure all of the attributes "pass"
-        
-        
-            // Create products if everything is "pass"
-    
+    $prod_query =  "SELECT * FROM tl_salsify_product ORDER BY id ASC";
+    $prod_result = $dbh->query($prod_query);
+    if($prod_result) {
+        while($prod = $prod_result->fetch_assoc()) {
+
+            // Fill in the rest of the product's information then create the product
+            $prod_values['tstamp'] = time();
+            $prod_values['dateAdded'] = time();
+            $prod_values['type'] = 5;
+            $prod_values['orderPages'] = 'a:1:{i:0;s:3:"109";}';
+            $prod_values['alias'] = $prod_values['item_number'];
+            $prod_values['name'] = $prod_values['specific_product_title'];
+            $prod_values['sku'] = $prod_values['item_number'];
+            $prod_values['description'] = $prod_values['full_description'];
+            $prod_values['published'] = 1;
+            $prod_values['upc'] = $prod_values['package_upc'];
+            $prod_values_result = \Database::getInstance()->prepare("INSERT INTO tl_iso_product %s")
+             ->set($prod_values)
+             ->execute();
+                                             
+             // Second, create entry in the 'tl_product_price' table                    
+            $price = array();
+            $price['pid'] = $prod_values_result->insertId;
+            $price['tstamp'] = time();
+            $price['tax_class'] = 1;
+            $price['config_id'] = 0;
+            $price['member_group'] = 0;
+            $priceResult = \Database::getInstance()->prepare("INSERT INTO tl_iso_product_price %s")
+             ->set($price)
+             ->execute();                                           
+                                                     
+            // First, create entry in the 'tl_product_pricetier" table
+            $priceTier = array();
+            $priceTier['pid'] = $priceResult->insertId;
+            $priceTier['tstamp'] = time();
+            $priceTier['min'] = 1;
+            $priceTier['price'] = '1.00';
+            $priceTierResult = \Database::getInstance()->prepare("INSERT INTO tl_iso_product_pricetier %s")
+             ->set($priceTier)
+             ->execute();
+ 
+        }
+    }
     
     echo "success";
