c318f73c219fb9b06c9a038f08967262d7fdc672
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-02-07 10:33:22 -0500

    Update tl_salsify_attribute.php

--- src/Resources/contao/dca/tl_salsify_attribute.php
+++ src/Resources/contao/dca/tl_salsify_attribute.php
@@ -89,7 +89,7 @@ $GLOBALS['TL_DCA']['tl_salsify_attribute'] = array
     // Palettes
     'palettes' => array
     (
-        'default'                     => '{salsify_attribute_legend}, attribute_key, attribute_value; {options_legend}, linked_isotope_attribute, linked_isotope_attribute_option, isotope_product_type, is_sku, is_name; {options_legend}, site_category_field, category_parent_page, category_page;{error_log_legend}, error_log;'
+        'default'                     => '{salsify_attribute_legend}, attribute_key, attribute_value; {options_legend}, linked_isotope_attribute, linked_isotope_attribute_option, isotope_product_type, is_sku, is_name; {options_legend}, category_parent_page, category_reader_page, category_page;{error_log_legend}, error_log;'
     ),
  
     // Fields
@@ -172,15 +172,6 @@ $GLOBALS['TL_DCA']['tl_salsify_attribute'] = array
             'sql'                     => "int(10) unsigned default NULL"
         ),
         // Salsify Attribute Fields
-        'site_category_field' => array
-        (
-            'label'                   => &$GLOBALS['TL_LANG']['tl_salsify_attribute']['site_category_field'],
-            'inputType'               => 'checkbox',
-            'default'				  => '',
-            'eval'                    => array('multiple'=>false, 'chosen'=>true, 'tl_class'=>'w50'),
-            'sql'                     => "char(1) NOT NULL default ''"
-        ),
-        // Salsify Attribute Fields
         'is_sku' => array
         (
             'label'                   => &$GLOBALS['TL_LANG']['tl_salsify_attribute']['is_sku'],
@@ -201,6 +192,14 @@ $GLOBALS['TL_DCA']['tl_salsify_attribute'] = array
         'category_parent_page' => array
         (
             'label'                   => &$GLOBALS['TL_LANG']['tl_salsify_attribute']['category_parent_page'],
+            'inputType'               => 'pageTree',
+            'eval'                    => array('files'=>false, 'fieldType'=>'radio', 'multiple'=>true, 'tl_class'=>'w50'),
+			'sql'                     => "blob NULL",
+            'relation'                => array('table'=>'tl_page', 'type'=>'hasMany', 'load'=>'lazy')
+        ),
+        'category_reader_page' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_salsify_attribute']['category_reader_page'],
             'inputType'               => 'pageTree',
             'eval'                    => array('files'=>false, 'fieldType'=>'radio', 'multiple'=>true, 'tl_class'=>'w50'),
 			'sql'                     => "blob NULL",
