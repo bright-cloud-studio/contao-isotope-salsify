4432f41b1b6e13c89683b4eecc4918349be493bb
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-02-10 13:52:12 -0500

    Update action.create.product.php

--- src/Resources/public/php/action.create.product.php
+++ src/Resources/public/php/action.create.product.php
@@ -1,10 +1,9 @@
 <?php
 
-    // Initialize
+    // INITIALIZE STUFFS
     session_start();
     require_once $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';
     
-    // Database Connection
     $dbh = new mysqli("localhost", "ecomm2_user", '(nNFuy*d8O=aC@BDCh', "ecomm2_contao_413");
     if ($dbh->connect_error) {
     die("Connection failed: " . $dbh->connect_error);
@@ -12,6 +11,7 @@
     
     // Store all of our products in data
     $products = array();
+    
     
     
     // STAGE DATA
@@ -48,7 +48,6 @@
 
         }
     }
-    
     
     
     
