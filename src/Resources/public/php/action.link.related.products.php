c84f15bf1eb29b5a0bad7394d42a5ea9bcba1993
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-02-14 09:58:35 -0500

    Update action.link.related.products.php

--- src/Resources/public/php/action.link.related.products.php
+++ src/Resources/public/php/action.link.related.products.php
@@ -18,16 +18,23 @@
     }
 
 
-
     // Loop through all products
-    // Loop through the Salsify Products
-    $prod_query =  "SELECT * FROM tl_salsify_product ORDER BY id ASC";
+    $prod_query =  "SELECT * FROM tl_iso_product ORDER BY id ASC";
     $prod_result = $dbh->query($prod_query);
     if($prod_result) {
         while($prod = $prod_result->fetch_assoc()) {
             
             
             // See if it has an entry in tl_iso_related_products
+            $related_query =  "SELECT * FROM tl_iso_related_product ORDER BY id ASC";
+            $related_result = $dbh->query($related_query);
+            if($related_result) {
+                while($related = $prod_result->fetch_assoc()) {
+                    echo "PROD: " . $prod->id . "<br>";
+                    echo "RELATED: " . $related->id . "<br>";
+                    
+                }
+            }
     
             //If not, make one,
             
