d844f2d1baebd5ad13a608066e9ed85b7fe06bbc
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-03-13 10:38:50 -0400

    Update tl_salsify_request.php

--- src/Resources/contao/languages/en/tl_salsify_request.php
+++ src/Resources/contao/languages/en/tl_salsify_request.php
@@ -11,6 +11,5 @@ $GLOBALS['TL_LANG']['tl_salsify_request']['toggle']                     = array(
 
 /* Fields */
 $GLOBALS['TL_LANG']['tl_salsify_request']['salsify_request_legend']   = 'Salsify Request Details';
-$GLOBALS['TL_LANG']['tl_salsify_request']['name']                     = array('Request Name', 'An internal-only name used to identify in the Backend');
-$GLOBALS['TL_LANG']['tl_salsify_request']['category_parent_page']     = array('Category Parent Page', 'Select the parent Page that the category pages will be added as children to.');
-$GLOBALS['TL_LANG']['tl_salsify_request']['salsify_category_field']   = array('Salsify Category Field', 'Enter in plain text the KEY value we will use to create product category pages.');
+$GLOBALS['TL_LANG']['tl_salsify_request']['request_name']             = array('Request Name', 'An internal-only name used to identify in the Backend');
+$GLOBALS['TL_LANG']['tl_salsify_request']['source_folder']            = array('Source Folder', 'The folder where our Salsify files will be published. This folder will be polled for updates.');
