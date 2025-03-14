a2a669df2579a4011bddcaa6f24bf178174751e8
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-03-14 14:26:09 -0400

    Update action.salsify.request.update.php

--- src/Resources/public/php/action.salsify.request.update.php
+++ src/Resources/public/php/action.salsify.request.update.php
@@ -1,2 +1,13 @@
 <?php
-  echo "Bing Bong Noise";
+
+    // INITIALIZE STUFFS
+    session_start();
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';
+    
+    $dbh = new mysqli("localhost", "ecomm2_user", '(nNFuy*d8O=aC@BDCh', "ecomm2_contao_413");
+    if ($dbh->connect_error) {
+    die("Connection failed: " . $dbh->connect_error);
+    }
+    
+    
+    echo "Bing Bong Noise<br>";
