9d8809951396a5ba332895b2db82bb3a830696e1
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-03-18 15:39:25 -0400

    Update action.salsify.request.update.php

--- src/Resources/public/php/action.salsify.request.update.php
+++ src/Resources/public/php/action.salsify.request.update.php
@@ -1,21 +1,30 @@
 <?php
 
+    use Contao\StringUtil;
+
     // INITIALIZE STUFFS
     session_start();
     require_once $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';
     
-    $dbh = new mysqli("localhost", "ecomm2_user", '(nNFuy*d8O=aC@BDCh', "ecomm2_contao_413");
+    
+    $dbh = new mysqli("localhost", "ecom_user", 'I6aX,Ud-EYa^]P9u8g', "ecom_contao_4_13");
     if ($dbh->connect_error) {
     die("Connection failed: " . $dbh->connect_error);
     }
     
     
-    echo "Bing Bong Noise<br>";
-
-
-    // 1 - Loop through all Salsify Requests
-
-        // Process
-
-            // Create if Doesnt Exist
-            // Update if Exists
+    // Loop through the Salsify Products
+    $sr_query =  "SELECT * FROM tl_salsify_request ORDER BY id ASC";
+    $sr_result = $dbh->query($sr_query);
+    if($sr_result) {
+        while($request = $sr_result->fetch_assoc()) {
+            
+            
+            echo "<pre>";
+            print_r($request);
+            echo "</pre><br><hr><br>";
+            
+            
+            
+        }
+    }
