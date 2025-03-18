7b0b66e9ef0964b86848a2889f957383c1ea3936
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-03-18 15:16:30 -0400

    Update tl_salsify_request.php

--- src/Resources/contao/languages/en/tl_salsify_request.php
+++ src/Resources/contao/languages/en/tl_salsify_request.php
@@ -12,7 +12,7 @@ $GLOBALS['TL_LANG']['tl_salsify_request']['toggle']                     = array(
 /* Fields */
 $GLOBALS['TL_LANG']['tl_salsify_request']['salsify_request_legend']   = 'Salsify Request Details';
 $GLOBALS['TL_LANG']['tl_salsify_request']['request_name']             = array('Request Name', 'An internal-only name used to identify in the Backend');
-$GLOBALS['TL_LANG']['tl_salsify_request']['source_folder']            = array('Source Folder', 'Enter the folder location for where to poll for updates. Assume we start in \'public_html/files\'');
+$GLOBALS['TL_LANG']['tl_salsify_request']['source_folder']            = array('Source Folder', 'Starting in \'public_html/files\', enter the folder location for where to poll for updates.');
 
 $GLOBALS['TL_LANG']['tl_salsify_request']['customization_legend']         = 'Customization Options';
 $GLOBALS['TL_LANG']['tl_salsify_request']['autolink_isotope_attributes']  = array('Autolink Isotope Attributes', 'By checking this box, when generating Salsify Attributes an attempt will be made to autolink these to Isotope Attributes where the field name matches the Attribute Key');
