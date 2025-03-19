f5da6392ddc646e45ea001e692220eb21856e83d
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-03-19 13:17:43 -0400

    Update tl_salsify_request.php

--- src/Resources/contao/languages/en/tl_salsify_request.php
+++ src/Resources/contao/languages/en/tl_salsify_request.php
@@ -13,9 +13,10 @@ $GLOBALS['TL_LANG']['tl_salsify_request']['toggle']                     = array(
 $GLOBALS['TL_LANG']['tl_salsify_request']['salsify_request_legend']   = 'Salsify Request Details';
 $GLOBALS['TL_LANG']['tl_salsify_request']['request_name']             = array('Request Name', 'An internal-only name used to identify in the Backend');
 $GLOBALS['TL_LANG']['tl_salsify_request']['source_folder']            = array('Source Folder', 'Starting in \'public_html/files\', enter the folder location for where to poll for updates.');
+$GLOBALS['TL_LANG']['tl_salsify_request']['latest_file']              = array('Latest File', 'The URL of the latest file we\'ve found.');
 
 $GLOBALS['TL_LANG']['tl_salsify_request']['customization_legend']         = 'Customization Options';
 $GLOBALS['TL_LANG']['tl_salsify_request']['autolink_isotope_attributes']  = array('Autolink Isotope Attributes', 'By checking this box, when generating Salsify Attributes an attempt will be made to autolink these to Isotope Attributes where the field name matches the Attribute Key');
 
 $GLOBALS['TL_LANG']['tl_salsify_request']['internal_information']    = 'Internal Information';
-$GLOBALS['TL_LANG']['tl_salsify_request']['last_update']             = array('Last Update', 'Date and Time of when the last update was performed for this request');
+$GLOBALS['TL_LANG']['tl_salsify_request']['flag_update']             = array('Flag - Update', 'When a new file is found, this flag will be checked and it will process during the next update loop.');
