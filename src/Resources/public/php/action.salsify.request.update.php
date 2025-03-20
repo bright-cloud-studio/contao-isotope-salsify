a39a78d91940b0218fa1c0fffca433f456b85e80
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-03-20 10:54:53 -0400

    Update action.salsify.request.update.php

--- src/Resources/public/php/action.salsify.request.update.php
+++ src/Resources/public/php/action.salsify.request.update.php
@@ -1,5 +1,13 @@
 <?php
 
+    use Bcs\Model\SalsifyAttribute;
+    use Bcs\Model\SalsifyProduct;
+    use Bcs\Model\SalsifyRequest;
+    
+    use pcrov\JsonReader\JsonReader;
+    
+    
+
     // INITS
     session_start();
     require_once $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';
@@ -17,11 +25,9 @@
         while($request = $sr_result->fetch_assoc()) {
 
 
-
             // STEP ONE - Determine which file is the latest
 
-            
-            
+
             // Build complete folder address
             $folder = $_SERVER['DOCUMENT_ROOT'] . "/../files/" . $request['source_folder'];
             
@@ -65,12 +71,74 @@
                 $dbh->prepare("UPDATE tl_salsify_request SET file_url='". $folder . "/" . $latest_file_url ."', file_date='" . $latest_file_date . "', flag_update='1' WHERE id='".$request['id']."'")->execute();
             }
 
-            // DEBUGS
-            echo "<hr><br>";
-
-
             
+
             // STEP TWO - Create or update Salsify Products and Salsify Attributes
+            
+            $reader = new JsonReader();
+            // Update to use Salsify Request file
+            $reader->open($folder . "/" . $latest_file_url);
+            $depth = $reader->depth();
+            $reader->read();
+            
+            $do_loop = 0;
+            // Process loaded XML data
+            do
+            {
+            	$do_loop++;
+            
+            	// Load the first array, which is the overall wrapper of arrays
+            	$array_parent = $reader->value();
+            
+            	$prod_count = 0;
+            	// Loop through children arrays, these are what store the actual values here
+            	foreach($array_parent as $array_child) {
+            		$prod_count++;
+            		
+            		// Create a Salsify Product to hold our Salsify Attributes
+            		$salsify_product = new SalsifyProduct();
+            		$salsify_product->tstamp = time();
+            		$salsify_product->product_sku = $do_loop . '_' . $prod_count;
+            		$salsify_product->name = 'product_' . $do_loop . '_' . $prod_count;
+            		$salsify_product->save();
+
+                    $attributes = array();
+                
+                    $prod_values = array();
+
+
+
+                    foreach($array_child as $key => $val) {
+                        
+                        $prod_values[$key] = $val[0];
+                        
+                        
+                        $salsify_attribute = new SalsifyAttribute();
+                        $salsify_attribute->pid = $salsify_product->id;
+                        $salsify_attribute->attribute_key = $key;
+                        $salsify_attribute->attribute_value = $val[0];
+                        $salsify_attribute->isotope_linked_attribute = null;
+                        $salsify_attribute->tstamp = time();
+                        $salsify_attribute->save();
+                        
+                        $attributes[$salsify_attribute->id]['key'] = $key;
+                        $attributes[$salsify_attribute->id]['value'] = $val[0];
+                        $log[$salsify_product->id]['attributes'] = $attributes;
+                        
+                    }
+
+            	}
+            
+            } while ($reader->next() && $reader->depth() > $depth); // Read each sibling.
+            
+            $reader->close();
+            
+            
+            
+            
+            
+            
+            
             
             
         
@@ -78,5 +146,7 @@
             
             
             
+            // DEBUGS
+            echo "<hr><br>";
         }
     }
