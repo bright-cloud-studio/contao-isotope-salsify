a5e36a02cf9a430fc63fbf7520686ae3f6856bce
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-04-16 13:51:57 -0400

    Update tl_salsify_attribute.php

--- src/Resources/contao/languages/en/tl_salsify_attribute.php
+++ src/Resources/contao/languages/en/tl_salsify_attribute.php
@@ -1,5 +1,8 @@
 <?php
 
+/* Custom Button on 'act'='edit' */
+$GLOBALS['TL_LANG']['tl_salsify_attribute']['link_similar']               = "Link Similar Attrs!";
+
 /* System Buttons */
 $GLOBALS['TL_LANG']['tl_salsify_attribute']['new']                        = array('New Salsify Attribute', 'Add a new record');
 $GLOBALS['TL_LANG']['tl_salsify_attribute']['show']                       = array('Salsify Attribute Details', 'Show the details of record ID %s');
@@ -7,7 +10,6 @@ $GLOBALS['TL_LANG']['tl_salsify_attribute']['edit']                       = arra
 $GLOBALS['TL_LANG']['tl_salsify_attribute']['copy']                       = array('Copy Salsify Attribute', 'Copy record ID %s');
 $GLOBALS['TL_LANG']['tl_salsify_attribute']['delete']                     = array('Delete Salsify Attribute', 'Delete record ID %s');
 $GLOBALS['TL_LANG']['tl_salsify_attribute']['toggle']                     = array('Toggle Salsify Attribute', 'Toggle record ID %s');
-
 
 /* Fields */
 $GLOBALS['TL_LANG']['tl_salsify_attrbute']['salsify_attribute_legend']    = 'Salsify Attribute Details';
