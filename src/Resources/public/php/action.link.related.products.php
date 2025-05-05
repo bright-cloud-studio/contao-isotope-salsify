dcd7856255fe1d3d043ed2b81e211737cc7a6b2e
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-05-05 14:11:38 -0400

    Update action.link.related.products.php

--- src/Resources/public/php/action.link.related.products.php
+++ src/Resources/public/php/action.link.related.products.php
@@ -1,5 +1,7 @@
 <?php
 
+    use Bcs\Model\SalsifyRequest;
+
     // INITIALIZE STUFFS
     session_start();
     require_once $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';
@@ -11,44 +13,49 @@
     }
 
     $linked = array();
+    
+    // Get Salsify Requests that are in the 'awaiting_cat_linking' state
+    $salsify_requests = SalsifyRequest::findBy(['status = ?'], ['awaiting_iso_generation']);
+    if(!$salsify_requests) {
+        foreach ($salsify_requests as $sr)
+		{
 
-
-
-    // LOOP THROUGH PRODUCTS
-    $prod_query =  "SELECT * FROM tl_iso_product ORDER BY id ASC";
-    $prod_result = $dbh->query($prod_query);
-    if($prod_result) {
-        while($prod = $prod_result->fetch_assoc()) {
-            
-            $found = false;
-            
-            // Loop through all related product entries
-            $related_query =  "SELECT * FROM tl_iso_related_product ORDER BY id ASC";
-            $related_result = $dbh->query($related_query);
-            if($related_result) {
-                while($related = $related_result->fetch_assoc()) {
-                    // If one of our entries matches 
-                    if($prod['id'] == $related['pid'])
-                        $found = true;
+            // LOOP THROUGH PRODUCTS
+            $prod_query =  "SELECT * FROM tl_iso_product WHERE pid='".$sr->id."' ORDER BY id ASC";
+            $prod_result = $dbh->query($prod_query);
+            if($prod_result) {
+                while($prod = $prod_result->fetch_assoc()) {
+                    
+                    $found = false;
+                    
+                    // Loop through all related product entries
+                    $related_query =  "SELECT * FROM tl_iso_related_product ORDER BY id ASC";
+                    $related_result = $dbh->query($related_query);
+                    if($related_result) {
+                        while($related = $related_result->fetch_assoc()) {
+                            // If one of our entries matches 
+                            if($prod['id'] == $related['pid'])
+                                $found = true;
+                        }
+                    }
+                    
+                    // If we didnt find a related products entry yet, make one
+                    if(!$found) {
+                        $cleaned = str_replace(' ', '', $prod['related_products']);
+                        $cleaned = explode(",",$cleaned);
+        
+                        $linked[$prod['id']] = $cleaned;
+                    }
                 }
             }
             
-            // If we didnt find a related products entry yet, make one
-            if(!$found) {
-                $cleaned = str_replace(' ', '', $prod['related_products']);
-                $cleaned = explode(",",$cleaned);
-
-                $linked[$prod['id']] = $cleaned;
-            }
-        }
+            // Update the status of our Salsify Request and save it
+            $sr->status = 'awaiting_new_file';
+            $sr->save();
+    
+		}
     }
-    
-    
-    // DEBUG
-    //echo "<pre>";
-    //print_r($linked);
-    //echo "</pre>";
-    //die();
+
     
     
     // Loop through $linked
@@ -56,8 +63,6 @@
         
         $ids = array();
         foreach($skus as $sku) {
-            
-            
             
             $prod_query =  "SELECT * FROM tl_iso_product where sku='".$sku."' ORDER BY id ASC";
             $prod_result = $dbh->query($prod_query);
