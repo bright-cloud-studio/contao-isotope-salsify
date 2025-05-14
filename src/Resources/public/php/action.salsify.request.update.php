aa2694da371a97099d509415c40692d54ca08145
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-05-14 14:28:33 -0400

    Update action.salsify.request.update.php

--- src/Resources/public/php/action.salsify.request.update.php
+++ src/Resources/public/php/action.salsify.request.update.php
@@ -10,6 +10,10 @@
     
     // Tracks who to turn off at the end for Product Release to site
     $publish_tracker = array();
+    // Tracks product grouping
+    $group_counter = array();
+    $isotope_product_type = '';
+    $isotope_product_type_variant = '';
     
     // Stores log messages until the end
     $log_messages = '';
@@ -20,7 +24,7 @@
     require_once $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';
     
     // DATABASE CONNECTION
-    $dbh = new mysqli("localhost", "ecomm2_user", '(nNFuy*d8O=aC@BDCh', "ecomm2_contao_413");
+    $dbh = new mysqli("localhost", "ecom_user", 'I6aX,Ud-EYa^]P9u8g', "ecom_contao_4_13");
     if ($dbh->connect_error) {
         die("Connection failed: " . $dbh->connect_error);
     }
@@ -73,8 +77,13 @@
                 
                 $run_update = true;
                 $request['file_url'] = $latest_file_url;
-                $dbh->prepare("UPDATE tl_salsify_request SET file_url='". $latest_file_url ."', file_date='" . $latest_file_date . "', status='awaiting_cat_linking' WHERE id='".$request['id']."'")->execute();
+                $dbh->prepare("UPDATE tl_salsify_request SET file_url='". $latest_file_url ."', file_date='" . $latest_file_date . "', status='awaiting_auto_linking' WHERE id='".$request['id']."'")->execute();
             }
+
+
+
+
+
 
 
 
@@ -180,8 +189,23 @@
                                     // Update the attribute_value to this latest version
                                     $update_sa->attribute_value = $val[0];
                                     
+                                    
+                                    // If our found SalsifyAttribute
                                     if($update_sa->controls_published) {
                                         $publish_tracker[$update_sa->pid] = $update_sa->attribute_value;
+                                    }
+                                    
+                                    // If our found SalsifyAttribute is grouping
+                                    if($update_sa->is_grouping) {
+                                        
+                                        if($isotope_product_type == '') {
+                                            $isotope_product_type = $update_sa->isotope_product_type;
+                                            $isotope_product_type_variant = $update_sa->isotope_product_type_variant;
+                                        }
+                                        
+                                        $group_counter[$update_sa->attribute_value] = $group_counter[$update_sa->attribute_value] + 1;
+                                        $salsify_product->variant_group = $update_sa->attribute_value;
+                                        $salsify_product->save();
                                     }
                                     
                                     // if we have an isotope attribute linked
@@ -265,97 +289,55 @@
                                     $salsify_attribute->attribute_key = $key;
                                     $salsify_attribute->attribute_value = $val[0];
                                     
-                                    // Auto-linking
-                                    
                                     // First, start off with out linked attribute being null
                                     $salsify_attribute->linked_isotope_attribute = null;
                                     
-                                    fwrite($myfile, "Attempting auto-link for new SalsifyAttribute \n");
                                     
-                                    // Now, try and find another SalsifyAttribute with the same key that has a linked attribute
-                                    $autolink_sa = SalsifyAttribute::findOneBy(['tl_salsify_attribute.attribute_key=?', 'tl_salsify_attribute.linked_isotope_attribute!=?'],[$key, null]);
-                                    if($autolink_sa) {
-                                        fwrite($myfile, "Found similar KEY with linked Isotope Attribute \n");
-                                        
-                                        // If we find one, apply it to our attribute
-                                        $salsify_attribute->linked_isotope_attribute = $autolink_sa->linked_isotope_attribute;
-                                        
                                     
-                                        // If our found SalsifyAttribute has a linked option, we can infur we need one here too
-                                        if($autolink_sa->linked_isotope_attribute_option != null) {
-                                            
-                                            fwrite($myfile, "Autolink helper SalsifyAttribute found to have Attribute Option, attempting to link or generate one \n");
-                                            
-                                            // Store our option IDs until the end
-    		                                $option_ids = array();
-    		                                $attribute_values = explode(", ", $salsify_attribute->attribute_value);
-                                            // Loop through each CSV value
-                                            foreach($attribute_values as $val) {
-            		                            
-            		                            // Find all Options for this Attribute
-                                				$existing_options = AttributeOption::findByPid($salsify_attribute->linked_isotope_attribute);
-                                				$opt_found = false;
-                                				foreach($existing_options as $option) {
-                                					// If an Option's label matches our Attribute Value, it already exists
-                                					if($option->label == $val) {
-                                						$opt_found = true;
-                                						//$attribute->linked_isotope_attribute_option = $option->id;
-                                						$option_ids[] = $option->id;
-                                						fwrite($myfile, "Option Found: ".$option->id.", adding to option_ids array \n");
-                                					}
-                                				}
-                                				// If no Attribute Option is found, create it
-                                				if($opt_found != true) {
-                                					$new_option = new AttributeOption();
-                                					$new_option->pid = $salsify_attribute->linked_isotope_attribute;
-                                					$new_option->label = $val;
-                                					$new_option->tstamp = time();
-                                					$new_option->published = 1;
-                                					$new_option->ptable = 'tl_iso_attribute';
-                                					$new_option->type = 'option';
-                                					
-                                					// Sorting
-                                					if($salsify_attribute->attribute_option_sorting == 'sort_numerical') {
-                                						// Strip everything but numbers from label, use that as sorting number
-                                						$only_number = preg_replace("/[^0-9]/","", $val);
-                                						$new_option->sorting = $only_number;
-                                						
-                                					} else if($salsify_attribute->attribute_option_sorting == 'sort_alphabetical') {
-                                						// Get just the first letter of the label, convert to number in alphabet, use as sorting number
-                                						$alphabet = range('A', 'Z');
-                                						$only_letter = substr($val, 0);
-                                						
-                                						$new_option->sorting = $alphabet[$only_letter];
-                                					}
-                
-                                					$new_option->save();
-                                					
-                                					$option_ids[] = $new_option->id;
-                            						fwrite($myfile, "New Option Created: ".$new_option->id.", adding to option_ids array \n");
-                                				}
-            		                            
-            		                        }
-                                            
-                                            $update_sa->linked_isotope_attribute_option = serialize($option_ids);
-                					        fwrite($myfile, "Saving Linked Attribute Option serialized array \n");
-                                        }
+                                    
+                                    
+                                    
+                                    
+                                    // TRY TO APPLY OUR SPECIALIZED SETTINGS FROM OTHER ATTRIBUTES
+                                    
+                                    // CONTROLS PUBLISHING
+                                    $sa_controls_published = SalsifyAttribute::findOneBy(['tl_salsify_attribute.attribute_key=?', 'tl_salsify_attribute.controls_published=?'],[$key, 1]);
+                                    if($sa_controls_published) {
+                                        $publish_tracker[$salsify_attribute->pid] = $salsify_attribute->attribute_value;
+                                        $salsify_attribute->controls_published = 1;
                                     }
                                     
+                                    // GROUPING
+                                    $sa_grouping = SalsifyAttribute::findOneBy(['tl_salsify_attribute.attribute_key=?', 'tl_salsify_attribute.is_grouping=?'],[$key, 1]);
+                                    if($sa_grouping) {
+                                        if($isotope_product_type == '') {
+                                            $isotope_product_type = $sa_grouping->isotope_product_type;
+                                            $isotope_product_type_variant = $sa_grouping->isotope_product_type_variant;
+                                        }
+                                        
+                                        $group_counter[$salsify_attribute->attribute_value] = $group_counter[$salsify_attribute->attribute_value] + 1;
+                                        $salsify_attribute->is_grouping = true;
+                                        $salsify_attribute->isotope_product_type = $sa_grouping->isotope_product_type;
+                                        $salsify_attribute->isotope_product_type_variant = $sa_grouping->isotope_product_type_variant;
+                                        
+                                        $salsify_product->variant_group = $salsify_attribute->attribute_value;
+                                        $salsify_product->save();
+                                        
+                                    }
                                     
-                                    // Try and apply settings matching other similar SalsifyAttributes
-                                    
-                                    
+                                    // IS CAT
+                                    $sa_is_cat = SalsifyAttribute::findOneBy(['tl_salsify_attribute.attribute_key=?', 'tl_salsify_attribute.is_cat=?'],[$key, 1]);
+                                    if($sa_is_cat) {
+                                        $salsify_attribute->is_cat = 1;
+                                    }
+
+
+
+
+
+
                                     $salsify_attribute->tstamp = time();
                                     $salsify_attribute->published = 1;
-                                    
-                                    // NEW PRODUCT //
-                                    
-                                    // Attempt to apply grouping
-                                    
-                                    // Attempt to apply is_cat
-                                    
-                                    // Attempt to apply controls_publish
-                                    
                                     $salsify_attribute->save();
                                     
                                     fwrite($myfile, "NEW Salsify Attribute ID: ".$salsify_attribute->id."\n");
@@ -387,6 +369,28 @@
                 fwrite($myfile, "No new file found, skipping generation/update \n");
             }
             
+            
+            fwrite($myfile, print_r($group_counter, true));
+            
+            // GROUPING
+            if($group_counter != null) {
+                fwrite($myfile, "Grouping SalsifyProducts \n\n");
+                $salsify_products = SalsifyProduct::findAll();
+                foreach($salsify_products as $prod) {
+                    
+                    if($group_counter[$prod->variant_group] == 1) {
+                        $prod->isotope_product_variant_type = 'single';
+                        $prod->isotope_product_type = $isotope_product_type;
+                        fwrite($myfile, "SalsifyProduct ID: " . $prod->id . " set as 'single' using Isotope Product Type ID: " . $isotope_product_type . "\n\n");
+                    } else {
+                        $prod->isotope_product_variant_type = 'variant';
+                        $prod->isotope_product_type = $isotope_product_type_variant;
+                        fwrite($myfile, "SalsifyProduct ID: " . $prod->id . " set as 'variant' using Isotope Product Type ID: " . $isotope_product_type_variant . "\n\n");
+                    }
+                    $prod->isotope_product_type_linked = 'linked';
+                    $prod->save();
+                }
+            }
             
             
             // At the end of the Salsify Request, we want to turn off things with $publish_tracker
