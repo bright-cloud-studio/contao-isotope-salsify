bab94e6159815ee485f9becc73778d5aa2cf702b
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-12-14 18:41:54 -0500

    Update 2.group.products.php

--- src/Resources/public/php/2.group.products.php
+++ src/Resources/public/php/2.group.products.php
@@ -31,8 +31,11 @@
     // STAGE DATA //
     ////////////////
     
+    $grouping_counter = array();
+    
+    
     // Loop through Salsify Requests on Step Two
-    $salsify_requests = SalsifyRequest::findBy(['status = ?'], ['awaiting_auto_linking']);
+    $salsify_requests = SalsifyRequest::findBy(['status = ?'], ['awaiting_grouping']);
     if($salsify_requests) {
         foreach ($salsify_requests as $sr)
 		{
@@ -41,25 +44,45 @@
 		    // Loop through Salsify Products that belong to this Salsify Request
 		    $salsify_products = SalsifyProduct::findBy(['pid = ?'], [$sr->id]);
             if($salsify_products) {
+                
+                debug($debug_mode, $log, "\tProducts Found: " . count($salsify_products));
+                
                 foreach ($salsify_products as $sp)
         		{
-        		    
-        		    debug($debug_mode, $log, "\t[Salsify Product ID: ".$sp->id."] Checking Salsify Attributes");
-        		    
-        		    // Loop through Salsify Attributes that belong to this Salsify Product
-        		    $salsify_attributes = SalsifyAttribute::findBy(['pid = ?', 'published = ?'], [$sp->id, 1]);
-                    if($salsify_attributes) {
-                        foreach ($salsify_attributes as $sa)
-                		{
-                		    // If we have a linked attribute, save it to our mapping
-                		    if($sa->linked_isotope_attribute == '') {
-                		        debug($debug_mode, $log, "\t\t[Salsify Attribute ID: ".$sa->id."][KEY: ".$sa->attribute_key."] Needs Linking");
-                		    }
-                		    
-                		}
+
+        		    // Find THIS products Salsify Attribute that controls grouping
+        		    $salsify_attribute = SalsifyAttribute::findOneBy(['pid = ?', 'is_grouping = ?'], [$sp->id, 1]);
+                    if($salsify_attribute) {
+                        
+                        // Find all Salsify Attributes that are grouping with the same value, that arent our kickoff one
+                        $others_in_group = SalsifyAttribute::findBy(['id != ?', 'attribute_value = ?', 'is_grouping = ?'], [$salsify_attribute->id, $salsify_attribute->attribute_value, 1]);
+                        if($others_in_group) {
+                            debug($debug_mode, $log, "\t[Salsify Product ID: " . $sp->id . "] [Group: ".$salsify_attribute->attribute_value."] Detected as Variant Product");
+                            
+                            // Salsify Product is Variant
+                            $sp->variant_group = $salsify_attribute->attribute_value;
+                            $sp->isotope_product_variant_type = 'variant';
+                            $sp->isotope_product_type = $sr->isotope_product_type_variant;
+                            $sp->save();
+                            
+                        } else {
+                            debug($debug_mode, $log, "\t[Salsify Product ID: " . $sp->id . "] Detected as Single Product");
+                            
+                            // Salsify Product is Single Product
+                            $sp->variant_group = $salsify_attribute->attribute_value;
+                            $sp->isotope_product_variant_type = 'single';
+                            $sp->isotope_product_type = $sr->isotope_product_type;
+                            $sp->save();
+                        }
+                        
+                        //$grouping_counter[$salsify_attribute->attribute_value] += 1;
+                        //debug($debug_mode, $log, "\t[Grouping: ".$salsify_attribute->attribute_value."] Total In Group: " . $grouping_counter[$salsify_attribute->attribute_value]);
                     }
-                    
+
         		}
+
+        		
+
             } else {
                 // Add a blank line between our Salsify Requests
                 debug($debug_mode, $log, "\tNo Salsify Products found");
@@ -71,109 +94,7 @@
 		}
     }
     
-    
-    ////////////////////////
-    // APPLY TO UN-LINKED //
-    ////////////////////////
-    
-    
-    // Loop through SalsifyAttributes
-    /*
-    $unlinked_attributes = SalsifyAttribute::findBy(['tl_salsify_attribute.linked_isotope_attribute IS null'], []);
-    if($unlinked_attributes) {
-        foreach ($unlinked_attributes as $unlinked_sa)
-		{
-		    $parent_salsify_product = SalsifyProduct::findOneBy(['id = ?'], [$unlinked_sa->pid]);
-            if($parent_salsify_product) {
-                
-                // See if we have a linking value in our mapping
-                if($mapping[$parent_salsify_product->isotope_product_type][$unlinked_sa->attribute_key] != '') {
 
-                    $unlinked_sa->linked_isotope_attribute = $mapping[$parent_salsify_product->isotope_product_type][$unlinked_sa->attribute_key];
-                    $unlinked_sa->status = "pass";
-                    
-                    ////////////////////////////////
-                    // AUTO-LINK ATTRIBUTE OPTION //
-                    ////////////////////////////////
-                    
-                    // Check if 'radio' or 'select'
-                    $iso_attr = Attribute::findBy(['id = ?'], [$unlinked_sa->linked_isotope_attribute]);
-                    if($iso_attr->type == 'select' || $iso_attr->type == 'radio') {
-                        
-                        // Store our option IDs until the end
-                        $option_ids = array();
-                        $attribute_values = explode(", ", $unlinked_sa->attribute_value);
-                        // Loop through each CSV value
-                        foreach($attribute_values as $val) {
-                            
-                            // Find all Options for this Attribute
-            				$existing_options = AttributeOption::findByPid($unlinked_sa->linked_isotope_attribute);
-            				$opt_found = false;
-            				foreach($existing_options as $option) {
-            					// If an Option's label matches our Attribute Value, it already exists
-            					if($option->label == $val) {
-            						$opt_found = true;
-            						$option_ids[] = $option->id;
-            						
-            						debug($debug_mode, $log, "Option Found: ".$option->id.", adding to option_ids array");
-            					}
-            				}
-            				// If no Attribute Option is found, create it
-            				if($opt_found != true) {
-            					$new_option = new AttributeOption();
-            					$new_option->pid = $unlinked_sa->linked_isotope_attribute;
-            					$new_option->label = $val;
-            					$new_option->tstamp = time();
-            					$new_option->published = 1;
-            					$new_option->ptable = 'tl_iso_attribute';
-            					$new_option->type = 'option';
-            					
-            					// Sorting
-            					if($unlinked_sa->attribute_option_sorting == 'sort_numerical') {
-            						// Strip everything but numbers from label, use that as sorting number
-            						$only_number = preg_replace("/[^0-9]/","", $val);
-            						$new_option->sorting = $only_number;
-            						
-            					} else if($unlinked_sa->attribute_option_sorting == 'sort_alphabetical') {
-            						// Get just the first letter of the label, convert to number in alphabet, use as sorting number
-            						$alphabet = range('A', 'Z');
-            						$only_letter = substr($val, 0);
-            						
-            						$new_option->sorting = $alphabet[$only_letter];
-            					}
-
-            					$new_option->save();
-            					
-            					$option_ids[] = $new_option->id;
-            					
-            					debug($debug_mode, $log, "New Option Created: ".$new_option->id.", adding to option_ids array");
-            				}
-                            
-                        }
-                        
-                        $unlinked_sa->linked_isotope_attribute_option = serialize($option_ids);
-                        
-                        debug($debug_mode, $log, "Saving Linked Attribute Option serialized array");
-
-                    }
-                    
-                    
-                    $unlinked_sa->save();
-                }
-                
-            }
-		}
-    }
-    
-    // Loop through SalsifyRequests
-    if($salsify_requests) {
-        foreach ($salsify_requests as $sr)
-		{
-		    $sr->status = 'awaiting_cat_linking';
-            $sr->save();
-		}
-    }
-    */
 
     // Close our logfile
     if($debug_mode)
