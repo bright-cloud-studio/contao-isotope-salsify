891ffc11c9c44dbba64288dd4e2630cd504ac772
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-09-10 15:40:18 -0400

    Refactor debug function to debugStepSix

--- src/Resources/public/php/6.link.related.products.php
+++ src/Resources/public/php/6.link.related.products.php
@@ -30,45 +30,45 @@
     $salsify_requests = SalsifyRequest::findBy(['status = ?'], ['awaiting_related_linking']);
     if($salsify_requests) {
         
-	    debug("[SalsifyRequest on 'awaiting_related_linking' Found] Processing all Isotope Products");
+	    debugStepSix("[SalsifyRequest on 'awaiting_related_linking' Found] Processing all Isotope Products");
 
 	    // Empty the tl_iso_related_product table so we can start fresh
         $reset_related_product_query =  "TRUNCATE TABLE tl_iso_related_product;";
         $reset_related_product_result = $dbh->query($reset_related_product_query);
-        debug("[Table 'tl_iso_related_product'] Clearing existing database entries so we start fresh");
+        debugStepSix("[Table 'tl_iso_related_product'] Clearing existing database entries so we start fresh");
 
         $isotope_products = Product::findBy(['tl_iso_product.pid!=?'], [-1]);
         
         if($isotope_products) {
-            debug("[Isotope Products Found] Looping through to find Related Products");
+            debugStepSix("[Isotope Products Found] Looping through to find Related Products");
             foreach($isotope_products as $isotope_product) {
-                debug("[Isotope Product ID: " . $isotope_product->id . "] Searching for Related Products", 1);
+                debugStepSix("[Isotope Product ID: " . $isotope_product->id . "] Searching for Related Products", 1);
                 
                 // Get our Related Product SKUs, add them to $linked array for later processing
                 $cleaned = str_replace(' ', '', $isotope_product->related_products);
                 $cleaned = explode(",",$cleaned);
                 $linked[$isotope_product->id] = $cleaned;
                 
-                debug("[Isotope Product ID: " . $isotope_product->id . "] Adding SKUs to seek list: " . $isotope_product->related_products, 2);
+                debugStepSix("[Isotope Product ID: " . $isotope_product->id . "] Adding SKUs to seek list: " . $isotope_product->related_products, 2);
             }
         }
 
         // For each SalsifyRequest on this status, update them to the next step
-        foreach ($salsify_requests as $sr)
-		{
-		    debug("[SalsifyRequest ID: ".$sr->id."] Moving SalsifyRequest to the next step");
-		    $sr->status = 'awaiting_new_file';
-            $sr->save();
-		}
+        //foreach ($salsify_requests as $sr)
+		//{
+		//    debugStepSix("[SalsifyRequest ID: ".$sr->id."] Moving SalsifyRequest to the next step");
+		//    $sr->status = 'awaiting_new_file';
+        //    $sr->save();
+		//}
     }
 
     
-    debug("Looping through linked array", 3);
+    debugStepSix("Looping through linked array", 3);
     
     // Loop through $linked
     foreach($linked as $key => $skus) {
         
-        debug("[Isotope Product ID: " . $key . "] Seeking Related Products", 3);
+        debugStepSix("[Isotope Product ID: " . $key . "] Seeking Related Products", 3);
         
         $ids = array();
         foreach($skus as $sku) {
@@ -76,9 +76,9 @@
             $related_product = Product::findOneBy(['tl_iso_product.sku=?'], [$sku]);
             if($related_product) {
                 $ids[] = $related_product->id;
-                debug("[Seeking Isotope Product by SKU: " . $sku . "] [FOUND Related Isotope Product ID: " . $related_product->id . "]", 4);
+                debugStepSix("[Seeking Isotope Product by SKU: " . $sku . "] [FOUND Related Isotope Product ID: " . $related_product->id . "]", 4);
             } else {
-                debug("[Seeking Isotope Product by SKU: " . $sku . "] [NO Isotope Product found for this SKU!]", 5);
+                debugStepSix("[Seeking Isotope Product by SKU: " . $sku . "] [NO Isotope Product found for this SKU!]", 5);
             }
 
         }
@@ -103,7 +103,7 @@
         
     
     /** Helper Functions **/
-    function debug($message, $indent_level = 0) {
+    function debugStepSix($message, $indent_level = 0) {
         if(DEBUG_MODE) {
             $indent = str_repeat("\t", $indent_level);
             $message = $indent . $message;
