1377f8cc3a936714e5500614c482c84c48b00352
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-09-11 11:07:49 -0400

    Create tl_nc_notification.php

---
+++ src/Resources/contao/languages/en/tl_nc_notification.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @copyright  Bright Cloud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao Isotope Salsify
+ * @license    LGPL-3.0+
+ * @see        https://github.com/bright-cloud-studio/contao-isotope-salsify
+ */
+
+/* Notification Types */
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['salsify']            = 'Salsify';
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['salsify_stalled']    = array('Salsify Request stalled', 'Sent when a Salsify Request stays on the same step for too long, usually because a step failed partway through and keeps retrying.');
