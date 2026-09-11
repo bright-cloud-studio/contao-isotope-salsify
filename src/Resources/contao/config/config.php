be17e67de7838cb76f424254f29c237221d0be3d
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-09-11 11:07:17 -0400

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -25,3 +25,17 @@ $GLOBALS['FE_MOD']['salsify']['mod_salsify_status_update']    = 'Bcs\Module\ModS
 $GLOBALS['TL_MODELS']['tl_salsify_request']         = 'Bcs\Model\SalsifyRequest';
 $GLOBALS['TL_MODELS']['tl_salsify_product']         = 'Bcs\Model\SalsifyProduct';
 $GLOBALS['TL_MODELS']['tl_salsify_attribute']       = 'Bcs\Model\SalsifyAttribute';
+
+/* Notification Center - alert sent by step seven when a Salsify Request stalls */
+$GLOBALS['NOTIFICATION_CENTER']['NOTIFICATION_TYPE']['salsify']['salsify_stalled'] = array
+(
+    'recipients'           => array('admin_email'),
+    'email_subject'        => array('request_id', 'request_name', 'request_status', 'request_file', 'stalled_minutes'),
+    'email_text'           => array('request_id', 'request_name', 'request_status', 'request_file', 'stalled_minutes'),
+    'email_html'           => array('request_id', 'request_name', 'request_status', 'request_file', 'stalled_minutes'),
+    'email_sender_name'    => array('admin_email'),
+    'email_sender_address' => array('admin_email'),
+    'email_recipient_cc'   => array('admin_email'),
+    'email_recipient_bcc'  => array('admin_email'),
+    'email_replyTo'        => array('admin_email')
+);
